a3abbbad652f856aa9a87810986161082e9df8ed
zahidatayyab <zahidatayyab666@gmail.com>
2023-12-16 01:49:50 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,31 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="card card-width">
+        <img src="..." className="card-img-top" alt="..." />
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+        </div>
+      </div>
+      <p className="text-primary">.text-primary</p>
+      <p className="text-secondary">.text-secondary</p>
+      <p className="text-success">.text-success</p>
+      <p className="text-danger">.text-danger</p>
+      <p className="text-warning bg-dark">.text-warning</p>
+      <p className="text-info bg-dark">.text-info</p>
+      <p className="text-light bg-dark">.text-light</p>
+      <p className="text-dark">.text-dark</p>
+      <p className="text-body">.text-body</p>
+      <p className="text-muted">.text-muted</p>
+      <p className="text-white bg-dark">.text-white</p>
+      <p className="text-black-50">.text-black-50</p>
+      <p className="text-white-50 bg-dark">.text-white-50</p>
+    </div>
+  );
+}
+
+export default App;
